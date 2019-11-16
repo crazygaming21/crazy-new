ec52c9cd84e0bb93808e637b41c47872b1801f3d
tracyskyes <55486092+tracyskyes@users.noreply.github.com>
2019-11-15 22:45:51 -0800

    wdada
    wdawdawdaw

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 
 app.get('/', function(req, res) {
-  res.send('Howwwwwwwwwww');
+  res.send('Howwwwwwwwwwwwwwwwwwww');
 });
 
 app.use('/api', api);
