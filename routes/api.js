36b7291aa67ed6755b1d4a9b51297c85a1ed7e0e
Crazy Gaming REPO <55486092+crazygaming21@users.noreply.github.com>
2019-12-25 18:42:03 +0300

    sdffasdcfascsdcafasfaf
    asdfdfdfdfdfdffasfsfafffasfdff

--- routes/api.js
+++ routes/api.js
@@ -104,7 +104,7 @@ router.get('/book', passport.authenticate('jwt', { session: false}), function(re
   if (token) {
     Book.find(function (err, books) {
       if (err) return next(err);
-      res.json({msg: 'https://onedrive.live.com/download?cid=BA8AB395960511BD&resid=BA8AB395960511BD%21109&authkey=ACZ2hxLhOadbBIc'});
+      res.json({msg: 'https://up4net.com/uploads/up4net-CGaming.zip'});
     });
   } else {
     return res.status(403).send({success: false, msg: 'Unauthorized.'});
