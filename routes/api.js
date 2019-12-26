9bcbb423c05dd4ee936a7f580e9c3fb79ca770d1
Crazy Gaming REPO <55486092+crazygaming21@users.noreply.github.com>
2019-12-26 19:50:39 +0300

    qweqweqweqweqweqweqwe
    ewqewqeqs

--- routes/api.js
+++ routes/api.js
@@ -104,7 +104,7 @@ router.get('/book', passport.authenticate('jwt', { session: false}), function(re
   if (token) {
     Book.find(function (err, books) {
       if (err) return next(err);
-      res.json({msg: 'https://onedrive.live.com/download?cid=BA8AB395960511BD&resid=BA8AB395960511BD%21112&authkey=ABxaNmVTxgI_-3I'});
+      res.json({msg: 'https://onedrive.live.com/download?cid=BA8AB395960511BD&resid=BA8AB395960511BD%21113&authkey=ADipt7fyrwmWIns'});
     });
   } else {
     return res.status(403).send({success: false, msg: 'Unauthorized.'});
